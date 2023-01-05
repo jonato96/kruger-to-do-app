5fc3b03542aeacdd220603b547de10bcfaa7c97b
Jonathan <jonato96s@gmail.com>
2023-01-04 20:59:03 -0500

    Add useState and to get the value every time user types

--- to-do/src/components/TodoApp.jsx
+++ to-do/src/components/TodoApp.jsx
@@ -1,9 +1,17 @@
+import { useState } from "react"
+
 const TodoApp = () =>{
+    const [title, setTitle] = useState("")
+    function handleChange(e){
+        const value = e.target.value
+        setTitle(value)
+    }
     return(
         <div className="todoContainer">
             <form className="todoCreateForm">
-                <input className="todoInput"/>
-                <input className="buttonCreate" type="submit" value="Create ToDo" />
+                <input className="todoInput" onChange={handleChange}/>
+                <input className="buttonCreate" type="submit" value="Create ToDo"/>
+                {title}
             </form>
         </div>
     )
