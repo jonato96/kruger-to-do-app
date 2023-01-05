cd07ff5609a9a1e783ea816cc672584129e87d82
Jonathan <jonato96s@gmail.com>
2023-01-04 21:53:54 -0500

    edit transform element

--- to-do/src/components/Todo.jsx
+++ to-do/src/components/Todo.jsx
@@ -2,12 +2,38 @@ import { useState } from "react"
 
 const Todo = ({item}) =>{
     const [isEdit, setIsEdit] = useState(false)
+    const [newValue, setNewValue] = useState(item.title)
+    function handleSubmit(e){
+        e.preventDefault()
+    }
+    
+    function handleChange(e){
+        const value = e.target.value
+        setNewValue(value)
+    }
+    const FormEdit = () =>{
+        
+        return(
+            <form className="todoUpdateForm" onSubmit={handleSubmit}>
+                <input type="text" onChange={handleChange} value={newValue}/>
+                <button>Update</button>                
+            </form>
+        )
+    }
+    const TodoElement = () =>{
+        return(
+            <div className="todoInfo">
+                {item.title}
+                <button onClick={()=>setIsEdit(true)}>Editar</button>
+                <button>Delete</button>
+            </div>
+        )
+    }
     return(
-        <div>
-            {item.title}
-            <button>Editar</button>
-            <button>Delete</button>
+        <div className="todo">
+            {isEdit ? <FormEdit/> : <TodoElement/>}
         </div>
+        
     )
 }
 export default Todo
