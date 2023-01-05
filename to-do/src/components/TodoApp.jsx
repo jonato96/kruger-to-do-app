b1f53d947907a387adbddd403b960ce284b96a16
Jonathan <jonato96s@gmail.com>
2023-01-04 21:08:41 -0500

    Adding useState list to save todos

--- to-do/src/components/TodoApp.jsx
+++ to-do/src/components/TodoApp.jsx
@@ -2,16 +2,28 @@ import { useState } from "react"
 
 const TodoApp = () =>{
     const [title, setTitle] = useState("")
+    const [todos, setTodos] = useState([])
     function handleChange(e){
         const value = e.target.value
         setTitle(value)
     }
+
+    function handleSubmit(e){
+        e.preventDefault()
+        const newTodo = {
+            id: crypto.randomUUID(),
+            title: title,
+            complete: false
+        }
+        const temp = [...todos]
+        temp.unshift(newTodo)
+        setTodos(temp)
+    }
     return(
         <div className="todoContainer">
-            <form className="todoCreateForm">
+            <form className="todoCreateForm" onSubmit={handleSubmit}>
                 <input className="todoInput" onChange={handleChange}/>
-                <input className="buttonCreate" type="submit" value="Create ToDo"/>
-                {title}
+                <input className="buttonCreate" type="submit" value="Create ToDo" onClick={handleSubmit}/>                
             </form>
         </div>
     )
